353d97afb5c40856b7b4f4546a56d3d5b9d11957
R. Jordan Crouser <jcrouser@smith.edu>
2022-11-02 18:40:55 -0500

    Add files via upload

---
+++ Building.java
@@ -0,0 +1,112 @@
+import javax.management.RuntimeErrorException;
+
+public class Building {
+
+    protected String name;
+    protected String address;
+    protected int nFloors;
+    private int activeFloor = -1; // Default value indicating we are not inside this building
+
+    /* Default constructor */
+    public Building() {
+        this("<Name Unknown>", "<Address Unknown>", 1);
+    }
+
+    /* Overloaded constructor with address only */
+    public Building(String address) {
+        this(); // Call default constructor
+        this.address = address; // Override address
+    }
+
+    /* Overloaded constructor with name, address */
+    public Building(String name, String address) {
+        this(name, address, 1); // Call full constructor with hard-coded # floors
+    }
+
+    /* Full constructor */
+    public Building(String name, String address, int nFloors) {
+        if (name != null) { this.name = name; }
+        if (address != null) { this.address = address; } 
+        if (nFloors < 1) {
+            throw new RuntimeException("Cannot construct a building with fewer than 1 floor.");
+        }
+        this.nFloors = nFloors;
+    }
+
+    /* Accessors */
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public int getFloors() {
+        return this.nFloors;
+    }
+
+    /* Navigation methods */
+    public Building enter() {
+        this.activeFloor = 1;
+        System.out.println("You are now inside " + this.name + " on the ground floor.");
+        return this; // Return a pointer to the current building
+    }
+
+    public Building exit() {
+        if (this.activeFloor == -1) {
+            throw new RuntimeException("You are not inside this Building. Must call enter() before exit().");
+        }
+        if (this.activeFloor > 1) {
+            throw new RuntimeException("You have fallen out a window from floor #" +this.activeFloor + "!");
+        }
+        System.out.println("You have left " + this.name + ".");
+        return null; // We're outside now, so the building is null
+    }
+
+    public void goToFloor(int floorNum) {
+        if (this.activeFloor == -1) {
+            throw new RuntimeException("You are not inside this Building. Must call enter() before navigating between floors.");
+        }
+        if (floorNum < 1 || floorNum > this.nFloors) {
+            throw new RuntimeException("Invalid floor number. Valid range for this Building is 1-" + this.nFloors +".");
+        }
+        System.out.println("You are now on floor #" + floorNum + " of " + this.name);
+        this.activeFloor = floorNum;
+    }
+
+    public void goUp() {
+        this.goToFloor(this.activeFloor + 1);
+    }
+
+    public void goDown() {
+        this.goToFloor(this.activeFloor - 1);
+    }
+
+    public void showOptions() {
+        System.out.println("Available options at " + this.name + ":\n + enter() \n + exit() \n + goUp() \n + goDown()\n + goToFloor(n)");
+    }
+
+    public String toString() {
+        return this.name + " is a " + this.nFloors + "-story building located at " + this.address + ".";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("------------------------------------");
+        System.out.println("Test of Building constructor/methods");
+        System.out.println("------------------------------------");
+        
+        Building fordHall = new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4);
+        System.out.println(fordHall);
+        fordHall.showOptions();
+
+        System.out.println("-----------------------------------");
+        System.out.println("Demonstrating enter/exit/navigation");
+        System.out.println("-----------------------------------");
+        fordHall.enter();
+        fordHall.goUp();
+        fordHall.goDown();
+        fordHall.exit();
+    }
+
+}
---
+++ CampusMap.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class CampusMap {
+
+    ArrayList<Building> buildings;
+
+    /* Default constructor, initializes empty ArrayList */
+    public CampusMap() {
+        buildings = new ArrayList<Building>();
+    }
+
+    /**
+     * Adds a Building to the map
+     * @param b the Building to add
+     */
+    public void addBuilding(Building b) {
+        System.out.println("Adding building...");
+        buildings.add(b);
+        System.out.println("-->Successfully added " + b.getName() + " to the map.");
+    }
+
+    /**
+     * Removes a Building from the map
+     * @param b the Building to remove
+     * @return the removed Building
+     */
+    public Building removeBuilding(Building b) {
+        System.out.println("Removing building...");
+        buildings.remove(b);
+        System.out.println("-->Successfully removed " + b.getName() + " to the map.");
+        return b;
+    }
+
+    public String toString() {
+        String mapString = "DIRECTORY of BUILDINGS";
+
+        for (int i = 0; i < this.buildings.size(); i ++) {
+            mapString += "\n  " + (i+1) + ". "+ this.buildings.get(i).getName() + " (" + this.buildings.get(i).getAddress() + ")";
+        }
+        return mapString;
+    }
+
+    public static void main(String[] args) {
+        CampusMap myMap = new CampusMap();
+        myMap.addBuilding(new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4));
+        myMap.addBuilding(new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4));
+        System.out.println(myMap);
+    }
+    
+}
